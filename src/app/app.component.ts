5670ca61eaee672f72b2913959e29e5cc510941d
Gabriel Chuquimia <chuquimiacopa.gabrie1@gmail.com>
2024-11-28 23:48:12 -0400

    template listo sin rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+///import { RouterOutlet } from '@angular/router';//
 import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [ HeaderComponent, FooterComponent, PortafolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
